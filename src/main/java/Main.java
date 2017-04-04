78ddc200bbeeef72a9d3f66f312a1a348c6a80b2
carlos andres sanchez v <carlos.sanchez-v@mail.escuelaing.edu.co>
2017-04-04 10:45:04 -0500

    prueba

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import com.zaxxer.hikari.HikariConfig;
+/*import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
@@ -8,14 +8,27 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-
+import java.util.Map;*/
 import static spark.Spark.*;
+import spark.*;
 
 public class Main {
-
-  public static void main(String[] args) {
-
+    
+    public static void main(String[] args){
+        port(Integer.valueOf(System.getenv("PORT")));
+        staticFileLocation("/public");
+        get("/hello",new Route() {
+            @Override
+            public Object handle(Request rqst, Response rspns) throws Exception {
+                String numero = rqst.queryParams("numero");
+                return "El cuadrado del numero es : " + Math.pow(Double.parseDouble(numero), 2);
+            }
+        });        
+        //get("hola", (request, response)->{
+        //    String numero = request.queryParams("numero");
+        //    return "El cuadrado del numero es: " + Math.pow(Double.parseDouble(numero), 2);});
+    }
+    /*
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
@@ -52,8 +65,8 @@ public class Main {
         attributes.put("message", "There was an error: " + e);
         return new ModelAndView(attributes, "error.ftl");
       }
-    }, new FreeMarkerEngine());
-
-  }
+    }, new FreeMarkerEngine());*/
 
 }
+
+
